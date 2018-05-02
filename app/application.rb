f18fc91543152613815d089c67fb61bbd87e0787
Nicholas Liang <ssss021418@gmail.com>
2018-05-02 23:01:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -20,7 +20,8 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/add/)
-      search_term = req.params["q"]
+      search_term = req.params["item"]
+      
     else
       resp.write "Path Not Found"
     end
