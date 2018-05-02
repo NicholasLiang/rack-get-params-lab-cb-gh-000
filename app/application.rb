dcbd46c2b09184b6c0bef013dc5e4ee3cce8e6a1
Nicholas Liang <ssss021418@gmail.com>
2018-05-02 23:05:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -17,7 +17,8 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       if @@cart.empty?
-        
+        puts ""
+      end
       @@cart.each do |item|
         resp.write "#{item}\n"
       end
