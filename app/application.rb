471da82d58e703260010564909402bb7e5f8c368
Nicholas Liang <ssss021418@gmail.com>
2018-05-02 22:58:46 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-    
+      @@cart.each do |item|
     else
       resp.write "Path Not Found"
     end
